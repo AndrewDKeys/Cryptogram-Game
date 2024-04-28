cab083047571dc81c5fe96e32392a8466a013779
Andrew <adkeys04@gmail.com>
2024-04-28 13:26:24 -0400

    Integrating server side part 1

--- cryptogram.c
+++ cryptogram.c
@@ -4,9 +4,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <netdb.h>
+#include <semaphore.h>
+#include <pthread.h>
 
-//Used for the lenght of encryption key, player key, and associated loops
-#define ALPHABET_LENGTH 26
+#define ALPHABET_LENGTH 26 //Used for the lenght of encryption key, player key, and associated loops
+#define BUFFER_SIZE 1024 //sufficient size for HTTP requests and responses
+#define PORT "8000"
+#define THREAD_COUNT 2
 
 char *answer; //Answer to the cryptogram
 char *decryptedAnswer;
@@ -14,6 +25,167 @@ char encryptionKey[ALPHABET_LENGTH];
 char playerKey[ALPHABET_LENGTH]; //Correct guesses that the player has made
 int listLength; //the length of the quotes
 struct quote *root; //root of linked list
+
+//structure for threading
+struct request {
+    pthread_t thread;
+    int client; //client's fd
+    char *path; //path to the html file
+    char *html;
+};
+
+void *handleRequest(void *request) {
+    struct request *clientRequest = (struct request *)request;
+    char *buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
+    int bytes = recv(clientRequest->client, buffer, BUFFER_SIZE, 0); //will put the http request into buffer
+    if(bytes == -1) {
+        close(clientRequest->client);
+        perror("request error");
+        pthread_exit(NULL);
+    }
+
+
+    //find the path being requested
+    char *saveptr;
+    char *token = strtok_r(buffer, " ", &saveptr);
+    for(int i = 0; token != NULL && i < 1; i++) {
+        token = strtok_r(NULL, " ", &saveptr);
+    }
+    token = token + 1; //this gets rid of the first '/' so we can cat it later
+
+    //build the absolute path
+    char *savedirptr;
+    char *directoryWalk = strtok_r(token, "/", &savedirptr);
+    while(directoryWalk != NULL) {
+        printf("here\n");
+        if(strstr(directoryWalk, ".html") == NULL) { //seeing if this is not the html file
+            strcat(clientRequest->path, directoryWalk);
+            strcat(clientRequest->path, "/");
+            directoryWalk = strtok_r(NULL, "/", &savedirptr);
+        } else {
+            clientRequest->html = directoryWalk;
+            break; //we found the html file, we don't want that in our path
+        }
+    }
+
+    DIR *htmlDirectory = opendir(clientRequest->path);
+    if(htmlDirectory == NULL) {
+        free(clientRequest->path);
+        send(clientRequest->client, "HTTP/1.1 404 Not Found\r\nContent-Length: 33\r\n\r\n<h1>404 Directory Not Found</h1>", 200, 0);
+        close(clientRequest->client);
+        pthread_exit(NULL);
+    }
+
+    //begin to look through the open directory to find the html file
+    printf("Looking for html file\n");
+    char *htmlFile;
+    int fileSize;
+    struct dirent *entry;
+    while((entry = readdir(htmlDirectory)) != NULL) {
+        if(strstr(entry->d_name, clientRequest->html) != NULL) {
+            int file = open(entry->d_name, O_RDONLY);
+            if(file == -1) {
+                free(clientRequest->path);
+                send(clientRequest->client, "HTTP/1.1 404 Not Found\r\nContent-Length: 33\r\n\r\n<h1>404 File Not Found</h1>", 200, 0);
+                close(clientRequest->client);
+                pthread_exit(NULL);
+            }
+
+            struct stat statbuf;
+            fstat(file, &statbuf);
+            fileSize = statbuf.st_size;
+
+            htmlFile = (char *)malloc(fileSize + 1);
+            htmlFile[0] = ' ';
+            read(file, htmlFile, fileSize + 1);
+
+            close(file);
+            break; //we can stop looking now
+        }
+    }
+    if(htmlFile == NULL || fileSize <= 0) {  
+        send(clientRequest->client, "HTTP/1.1 404 Not Found\r\nContent-Length: 33\r\n\r\n<h1>404 File Not Found</h1>", 200, 0);
+        free(clientRequest->path);
+        close(clientRequest->client);    
+        closedir(htmlDirectory);
+        pthread_exit(NULL);
+    }
+
+    char response[50 + fileSize + 1]; //initializing enough room for the response
+    response[0] = ' ';
+    snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s", fileSize, htmlFile);
+    //strcat(response, htmlFile);
+
+    send(clientRequest->client, response, sizeof(response), 0);
+
+    free(htmlFile);
+    closedir(htmlDirectory);
+    free(clientRequest->path);
+    close(clientRequest->client);
+    pthread_exit(NULL);
+}
+
+void requestListen(int sockfd, const char *path) {
+    //set up any semaphores here
+    int client;
+    struct sockaddr_storage clientAddress;
+    socklen_t sockSize = sizeof(clientAddress);
+    while(1) {
+        client = accept(sockfd, (struct sockaddr *)&clientAddress, &sockSize);
+        if(client == -1) {
+            perror("accept request failure");
+            close(client);
+            close(sockfd);
+            exit(1);
+        }
+        
+        struct request newRequest;
+        newRequest.client = client;
+        newRequest.path = (char *)malloc(1024*sizeof(char)); //enough for 1k bytes
+        strcpy(newRequest.path, path);
+        pthread_create(&newRequest.thread, NULL, handleRequest, (void *)&newRequest);
+    }
+}
+
+//this will return a socketfd on succes or a -1 on failure
+int socketSetUp() {
+    struct addrinfo addressInfo, *serverInfo, *current; //current will be used for looping
+    memset(&addressInfo, 0, sizeof(addressInfo));
+    addressInfo.ai_family = AF_UNSPEC;
+    addressInfo.ai_socktype = SOCK_STREAM;
+    addressInfo.ai_flags = AI_PASSIVE;
+
+    if(getaddrinfo(NULL, PORT, &addressInfo, &serverInfo) == -1) {
+        freeaddrinfo(serverInfo);
+        freeaddrinfo(current);
+        perror("server address info error\n");
+        exit(1);
+    }
+
+    int sock;
+    for(current = serverInfo; current != NULL; current = current->ai_next) {
+        sock = socket(current->ai_family, current->ai_socktype, current->ai_protocol);
+        if(sock == -1) {
+            continue; //failed, trying another
+        }
+        if(bind(sock, current->ai_addr, current->ai_addrlen) == -1) {
+            perror("binding failed");
+            close(sock);
+            continue; //failed, trying another
+        }
+        break;
+    }
+    free(serverInfo);
+
+    //we will have 25 requests for every thread we are using
+    if(listen(sock, THREAD_COUNT*25) == -1) {
+        close(sock);
+        perror("sock listen failure");
+        exit(1);
+    }
+
+    return sock;
+}
 
 //Uses the Fisher-Yates method to shuffle our encryption key
 void shuffle(char encryption[ALPHABET_LENGTH]) {
@@ -231,13 +403,27 @@ void teardown() {
 }
 
 int main(int argc, char *argv[]) {
-  do {
-    initialization();
-    gameLoop();
-    teardown();
-    printf("Would you like to play again? [y/n]\n");
-  } while(playAgain());
-  freeList();
+    if(argc != 2) {
+        printf("failure: no path provided\n");
+        return EXIT_FAILURE;
+    }
+
+    int sockfd = socketSetUp();
+    if(sockfd == -1) {
+        close(sockfd);
+        perror("network setup error\n");
+        return EXIT_FAILURE;
+    }
+
+    requestListen(sockfd, argv[1]);
+
+  // do {
+  //   initialization();
+  //   gameLoop();
+  //   teardown();
+  //   printf("Would you like to play again? [y/n]\n");
+  // } while(playAgain());
+  // freeList();
 
   return 0;
 }
